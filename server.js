b3adb91c717d31c6c93887266d90adda95296067
smil3cz <jan.melicherik@email.cz>
2021-03-17 15:34:58 +0100

    created basic model for tour

--- server.js
+++ server.js
@@ -18,6 +18,20 @@ mongoose
 
 const app = require('./app');
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name!'],
+    unique: true,
+  }, // SCHEMA TYPE OPTIONS
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price!'] },
+  difficulty: String,
+  premium: Boolean,
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
