ce97918153c3252ef86363a285118bf1534a47cd
smil3cz <jan.melicherik@email.cz>
2021-03-17 15:27:20 +0100

    setup basic connection to DB

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env' });
+
+const DB = process.env.DB_STRING.replace('<PASSWORD>', process.env.DB_PW);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connected!');
+  });
 
 const app = require('./app');
 
