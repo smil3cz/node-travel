ecb0e0b065968ac9f047a8d94025e8aced11a8b5
smil3cz <jan.melicherik@email.cz>
2021-04-02 15:40:36 +0200

    created geospatials data

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -20,6 +20,13 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+// Geospatial queries
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/')
   .get(tourController.getAllTours)
